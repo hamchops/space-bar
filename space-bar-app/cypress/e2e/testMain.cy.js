c94da8c18070d38bcabe3fb4cdba540166a2afe5
lerenzo martinez <martinezlerenzo@gmail.com>
2024-04-01 17:17:20 -0500

    format profile details via css. implemented test to ensure profile details list exist/renders

--- space-bar-app/cypress/e2e/testMain.cy.js
+++ space-bar-app/cypress/e2e/testMain.cy.js
@@ -3,5 +3,7 @@ describe('template spec', () => {
     cy.visit('http://localhost:3000')
     
     cy.get('[data-testid="cypress-home-title"]').should("exist")
+    .get('div')
+    .should('have.class', 'profileDetails')
   })
 })
--- space-bar-app/src/components/Home.jsx
+++ space-bar-app/src/components/Home.jsx
@@ -3,8 +3,19 @@ import React from 'react'
 
 function Home (props) {
     return(
-        <h1 data-testid="cypress-home-title">Hello this is home</h1>
+        <div>
+            <h1 className='title' data-testid="cypress-home-title">Hello this is home</h1>
 
+            <div className="profileDetails" data-testid="cypress-profile-details">
+                <ul className="profileList">
+                    <li><h3 className='userID'>user ID:</h3></li>
+                    <li><h3 className='email'>Email:</h3></li>
+                    <li><h3 className='uriLine'>Spotify URI:</h3></li>
+                    <li><h3 className='link'>Link</h3></li>
+                    <li><h3 className='profilePic'>Profile Image</h3></li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
